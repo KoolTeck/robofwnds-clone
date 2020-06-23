947b0e3d01ff25574232dabd319ec21abeaf99f1
Adetunji Adebowale <66047485+KoolTeck@users.noreply.github.com>
2020-06-23 19:05:05 +0100

    updated cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({name, email, id}) => {
     return (
-        <div className= 'bg-light-green dib br3 pa2 ma2 grow bw2 shadow-3 tc'>
+        <div className= 'bg-light-green dib br3 pa2 ma1 grow bw6 shadow-3 tc'>
            <img alt='robots' src={`https://robohash.org/${id}?`} />
 
             <div>
